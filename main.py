947f8002951893a1267378b93844dd70d0092400
KingOfMath <823221784@qq.com>
2020-02-22 21:23:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 import xlsxParser
 import graph_tools as gt
 import numpy as np
+import networkx as nx
 
 g = gt.Graph(directed=True)
+nxG = nx.Graph()
 timestamps = xlsxParser.parser()
 
 # --------------------------------------------------------
@@ -15,17 +17,33 @@ for link in timestamps:
     node_out = int(link[1])
     if not g.has_edge(node_in, node_out):
         g.add_edge(node_in, node_out)
+    if not nxG.has_edge(node_in, node_out):
+        nxG.add_edge(node_in, node_out)
 
 # --------------------------------------------------------
 
 # ******
-# Q 1
+# Q 1 : N,L,p,ED,VarD
 # ******
 N = len(g.vertices())
 L = len(g.edges())
+p = nx.density(nxG)
+ED = []
+VarD = []
 
 # ******
-# Q 5
+# Q 3 : pD => -0.29 ???
+# ******
+pD = nx.degree_assortativity_coefficient(nxG)
+
+# ******
+# Q 4 : cc
+# ******
+cc_dict = nx.clustering(nxG)
+cc = np.sum(cc_dict[i] for i in range(1, N)) / N
+
+# ******
+# Q 5 : EH,Hmax
 # comment line 585
 # ******
 betweenness = 0
@@ -34,15 +52,20 @@ for i in range(1, N):
     betweenness += betweenness_each
 EH = 2 * betweenness / (N * (N - 1))
 
+Hmax = 0
+for each in nx.shortest_path_length(nxG).items():
+    for _, v in each[1].items():
+        if v > Hmax:
+            Hmax = v
 
 # ******
-# Q 7  ??? => 0
+# Q 7 : max_adj_eigval => 0 ???
 # ******
 adj_matrix_eigvals = g.adjacency_matrix_eigvals()
 max_adj_eigval = adj_matrix_eigvals[-1]
 
 # ******
-# Q 8
+# Q 8 : second_min_lap_eigval
 # ******
 lap_matrix_eigvals = g.laplacian_matrix_eigvals()
 second_min_lap_eigval = lap_matrix_eigvals[1]
