56dba79f67e07ccfed4276e6eabd3e6fa9b43024
KingOfMath <823221784@qq.com>
2020-03-02 20:23:05 +0100

    Add files via upload

--- part_C.py
+++ part_C.py
@@ -8,22 +8,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pathpy as pp
 import jgraph
+from IPython.display import *
+from IPython.display import HTML
 
 # g = gt.Graph(directed=True)
 # nxG = nx.Graph()
-timestamps = xlsxParser.parser()
+raws = xlsxParser.parser()
 t = pp.TemporalNetwork()
 
 # --------------------------------------------------------
 # this graph works without timestamps
 # deemed as a final network on the last timestamp
-for link in timestamps:
+for link in raws:
     node_in = link[0]
     node_out = link[1]
     t.add_edge(source=node_in,target=node_out,ts=int(link[2]))
 
 # --------------------------------------------------------
 
+# work only in HTML
 jgraph.draw(t)
+plt.show()
 
+# ******
+# Q 14
+# ******
+
+
+# ******
+# Q 15
+# ******
 
