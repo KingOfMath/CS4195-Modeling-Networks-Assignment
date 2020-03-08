fd0c2b9af0d8df30c0688995a39323341b47ab93
KingOfMath <823221784@qq.com>
2020-03-08 21:07:14 +0100

    Add files via upload

--- part_B.py
+++ part_B.py
@@ -34,40 +34,41 @@ for link in raws:
 # Q 9: Eit, Vit
 # ******
 
-# Eit = []
-# Vit = []
-# Nodes = []
-# Infected = []
-# Eit_seed = []
-# Vit_seed = []
-# total = 0
-# timestamp = 0
-# for link in raws:
-#     node_in = int(link[0])
-#     node_out = int(link[1])
-#     currentTime = int(link[2])
-#     if currentTime == timestamp + 1:
-#         if (node_in == 1) or (node_in in Infected):
-#             if node_out not in Infected:
-#                 total += 1
-#                 Infected.append(node_out)
-#     else:
-#         timestamp += 1
-#         if (node_in == 1) or (node_in in Infected):
-#             if node_out not in Infected:
-#                 total += 1
-#                 Infected.append(node_out)
-#         Nodes.append(total)
-#         Eit_seed.append(total/167)
-#         Vit_seed.append(np.std(Nodes))
+Eit = []
+Vit = []
+Nodes = []
+Infected = []
+Eit_seed = []
+Vit_seed = []
+total = 0
+timestamp = 0
+for link in raws:
+    node_in = int(link[0])
+    node_out = int(link[1])
+    currentTime = int(link[2])
+    if currentTime == timestamp + 1:
+        if (node_in == 1) or (node_in in Infected):
+            if node_out not in Infected:
+                total += 1
+                Infected.append(node_out)
+    else:
+        timestamp += 1
+        if (node_in == 1) or (node_in in Infected):
+            if node_out not in Infected:
+                total += 1
+                Infected.append(node_out)
+        Nodes.append(total)
+        Eit_seed.append(total/167)
+        # Vit_seed.append(np.std(Nodes))
+print(len(Eit_seed))
 # plt.figure()
 # # plt.subplot(121)
 # plt.xlabel('time stamp')
 # plt.ylabel('standard varience of infected nodes')
 # plt.plot(range(57790), Vit_seed)
-# # plt.subplot(122)
-# # plt.plot(range(57790), Vit)
-# plt.show()
+# plt.subplot(122)
+# plt.plot(range(57790), Vit)
+plt.show()
 
 # ******
 # Q 10: R
@@ -116,136 +117,136 @@ for link in raws:
 #     R.append(i)
 # for i, j in sorted(notUsed, key=lambda item: item[1])[::-1]:
 #     R.append(i)
-
-
-# ******
-# Q 11: C,D,rRD,rRC
-# ******
-
-# D = []
-# C = []
-# degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
-# cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
-# for a in degree_arr:
-#     D.append(a[0])
-# for a in cluster_arr:
-#     C.append(a[0])
 #
-# rRD = []
-# rRC = []
-# all_f = np.linspace(0.05, 0.5, 10)
-# for f in all_f:
-#     fN = int(f * 167)
-#     RD = []
-#     RC = []
-#     for node in R[:fN]:
-#         if node in D[:fN]:
-#             RD.append(node)
-#         if node in C[:fN]:
-#             RC.append(node)
-#     rRD.append(len(RD) / fN)
-#     rRC.append(len(RC) / fN)
 #
-# plt.figure()
-# plt.subplot(121)
-# plt.xlabel('f')
-# plt.ylabel('rRD')
-# plt.plot(all_f, rRD)
-# plt.subplot(122)
-# plt.xlabel('f')
-# plt.ylabel('rRC')
-# plt.plot(all_f, rRC)
-# plt.show()
-
-# ******
-# Q 12 : P1 => treeRank; P2 => ?
-# ******
-
-# Infected = []
-# treeRank = []
-# timestamp = 0
-# t = Tree()
-# root = Node(1, 1, data=0)
-# t.add_node(root)
-# limit = int(167 * 0.8)
-# for link in raws:
-#     node_in = int(link[0])
-#     node_out = int(link[1])
-#     currentTime = int(link[2])
-#     if currentTime == timestamp + 1:
-#         if (node_in == 1) or (node_in in Infected):
-#             # only first time infected is used, otherwise it is repeated
-#             if node_out not in Infected:
-#                 Infected.append(node_out)
-#                 t.add_node(Node(node_out, node_out, data=1), parent=t.get_node(node_in))
-#                 iter_num = node_out
-#                 # update the infected chain to root
-#                 while t.parent(iter_num) is not None:
-#                     t.parent(iter_num).data += 1
-#                     iter_num = t.parent(iter_num).tag
-#     else:
-#         timestamp += 1
-#         if node_in in Infected:
-#             if node_out not in Infected:
-#                 Infected.append(node_out)
-#                 t.add_node(Node(node_out, node_out, data=1), parent=t.get_node(node_in))
-#                 iter_num = node_out
-#                 # iter
-#                 while t.parent(iter_num) is not None:
-#                     t.parent(iter_num).data += 1
-#                     iter_num = t.parent(iter_num).tag
+# # ******
+# # Q 11: C,D,rRD,rRC
+# # ******
 #
-# # jsonObj = t.to_json()
-# # print(jsonObj)
+# # D = []
+# # C = []
+# # degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
+# # cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
+# # for a in degree_arr:
+# #     D.append(a[0])
+# # for a in cluster_arr:
+# #     C.append(a[0])
+# #
+# # rRD = []
+# # rRC = []
+# # all_f = np.linspace(0.05, 0.5, 10)
+# # for f in all_f:
+# #     fN = int(f * 167)
+# #     RD = []
+# #     RC = []
+# #     for node in R[:fN]:
+# #         if node in D[:fN]:
+# #             RD.append(node)
+# #         if node in C[:fN]:
+# #             RC.append(node)
+# #     rRD.append(len(RD) / fN)
+# #     rRC.append(len(RC) / fN)
+# #
+# # plt.figure()
+# # plt.subplot(121)
+# # plt.xlabel('f')
+# # plt.ylabel('rRD')
+# # plt.plot(all_f, rRD)
+# # plt.subplot(122)
+# # plt.xlabel('f')
+# # plt.ylabel('rRC')
+# # plt.plot(all_f, rRC)
+# # plt.show()
 #
-# temp = []
-# for i in range(1, 168):
-#     tag = t.get_node(i).tag
-#     data = t.get_node(i).data
-#     temp.append((tag, data))
-# for i,j in sorted(temp,key=lambda item:item[1])[::-1]:
-#     treeRank.append(i)
+# # ******
+# # Q 12 : P1 => treeRank; P2 => ?
+# # ******
 #
-# D = []
-# C = []
-# degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
-# cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
-# for a in degree_arr:
-#     D.append(a[0])
-# for a in cluster_arr:
-#     C.append(a[0])
+# # Infected = []
+# # treeRank = []
+# # timestamp = 0
+# # t = Tree()
+# # root = Node(1, 1, data=0)
+# # t.add_node(root)
+# # limit = int(167 * 0.8)
+# # for link in raws:
+# #     node_in = int(link[0])
+# #     node_out = int(link[1])
+# #     currentTime = int(link[2])
+# #     if currentTime == timestamp + 1:
+# #         if (node_in == 1) or (node_in in Infected):
+# #             # only first time infected is used, otherwise it is repeated
+# #             if node_out not in Infected:
+# #                 Infected.append(node_out)
+# #                 t.add_node(Node(node_out, node_out, data=1), parent=t.get_node(node_in))
+# #                 iter_num = node_out
+# #                 # update the infected chain to root
+# #                 while t.parent(iter_num) is not None:
+# #                     t.parent(iter_num).data += 1
+# #                     iter_num = t.parent(iter_num).tag
+# #     else:
+# #         timestamp += 1
+# #         if node_in in Infected:
+# #             if node_out not in Infected:
+# #                 Infected.append(node_out)
+# #                 t.add_node(Node(node_out, node_out, data=1), parent=t.get_node(node_in))
+# #                 iter_num = node_out
+# #                 # iter
+# #                 while t.parent(iter_num) is not None:
+# #                     t.parent(iter_num).data += 1
+# #                     iter_num = t.parent(iter_num).tag
+# #
+# # # jsonObj = t.to_json()
+# # # print(jsonObj)
+# #
+# # temp = []
+# # for i in range(1, 168):
+# #     tag = t.get_node(i).tag
+# #     data = t.get_node(i).data
+# #     temp.append((tag, data))
+# # for i,j in sorted(temp,key=lambda item:item[1])[::-1]:
+# #     treeRank.append(i)
+# #
+# # D = []
+# # C = []
+# # degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
+# # cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
+# # for a in degree_arr:
+# #     D.append(a[0])
+# # for a in cluster_arr:
+# #     C.append(a[0])
+# #
+# # rRD = []
+# # rRC = []
+# # all_f = np.linspace(0.05, 0.5, 10)
+# # for f in all_f:
+# #     fN = int(f * 167)
+# #     fNR = int(f * len(treeRank))
+# #     RD = []
+# #     RC = []
+# #     for node in treeRank[:fNR]:
+# #         if node in D[:fN]:
+# #             RD.append(node)
+# #         if node in C[:fN]:
+# #             RC.append(node)
+# #     rRD.append(len(RD) / fNR)
+# #     rRC.append(len(RC) / fNR)
+# #
+# # plt.figure()
+# # plt.subplot(121)
+# # plt.xlabel('f')
+# # plt.ylabel('rTD')
+# # plt.plot(all_f, rRD)
+# # plt.subplot(122)
+# # plt.xlabel('f')
+# # plt.ylabel('rTC')
+# # plt.plot(all_f, rRC)
+# # plt.show()
 #
-# rRD = []
-# rRC = []
-# all_f = np.linspace(0.05, 0.5, 10)
-# for f in all_f:
-#     fN = int(f * 167)
-#     fNR = int(f * len(treeRank))
-#     RD = []
-#     RC = []
-#     for node in treeRank[:fNR]:
-#         if node in D[:fN]:
-#             RD.append(node)
-#         if node in C[:fN]:
-#             RC.append(node)
-#     rRD.append(len(RD) / fNR)
-#     rRC.append(len(RC) / fNR)
+# # ******
+# # Q 13: R_
+# # ******
 #
-# plt.figure()
-# plt.subplot(121)
-# plt.xlabel('f')
-# plt.ylabel('rTD')
-# plt.plot(all_f, rRD)
-# plt.subplot(122)
-# plt.xlabel('f')
-# plt.ylabel('rTC')
-# plt.plot(all_f, rRC)
-# plt.show()
-
-# ******
-# Q 13: R_
-# ******
-
 # d = {}
 # for i in range(1, samples):
 #     d[i] = {}
@@ -289,39 +290,60 @@ for link in raws:
 # avg_time = {k: int(v) for k, v in avg_time.items() if v != 0}
 # for i, j in sorted(dict2list(avg_time), key=lambda item: item[1]):
 #     R_.append(i)
+# #
+# # D = []
+# # C = []
+# # degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
+# # cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
+# # for a in degree_arr:
+# #     D.append(a[0])
+# # for a in cluster_arr:
+# #     C.append(a[0])
+# #
+# # rRD = []
+# # rRC = []
+# # all_f = np.linspace(0.05, 0.5, 10)
+# # for f in all_f:
+# #     fN = int(f * 167)
+# #     fNR = int(f * len(R_))
+# #     RD = []
+# #     RC = []
+# #     for node in R_[:fNR]:
+# #         if node in D[:fN]:
+# #             RD.append(node)
+# #         if node in C[:fN]:
+# #             RC.append(node)
+# #     rRD.append(len(RD) / fNR)
+# #     rRC.append(len(RC) / fNR)
+# #
+# # plt.figure()
+# # plt.subplot(121)
+# # plt.xlabel('f')
+# # plt.ylabel('rR\'D')
+# # plt.plot(all_f, rRD)
+# # plt.subplot(122)
+# # plt.xlabel('f')
+# # plt.ylabel('rR\'C')
+# # plt.plot(all_f, rRC)
+# # plt.show()
 #
-# D = []
-# C = []
-# degree_arr = sorted(nxG.degree, key=lambda item: item[1])[::-1]
-# cluster_arr = sorted(nx.clustering(nxG).items(), key=lambda item: item[1])[::-1]
-# for a in degree_arr:
-#     D.append(a[0])
-# for a in cluster_arr:
-#     C.append(a[0])
-#
-# rRD = []
-# rRC = []
+# rRR = []
 # all_f = np.linspace(0.05, 0.5, 10)
 # for f in all_f:
 #     fN = int(f * 167)
 #     fNR = int(f * len(R_))
-#     RD = []
-#     RC = []
+#     RCC = []
 #     for node in R_[:fNR]:
-#         if node in D[:fN]:
-#             RD.append(node)
-#         if node in C[:fN]:
-#             RC.append(node)
-#     rRD.append(len(RD) / fNR)
-#     rRC.append(len(RC) / fNR)
+#         if node in R[:fN]:
+#             RCC.append(node)
+#     rRR.append(len(RCC) / fNR)
 #
 # plt.figure()
-# plt.subplot(121)
 # plt.xlabel('f')
-# plt.ylabel('rR\'D')
-# plt.plot(all_f, rRD)
-# plt.subplot(122)
-# plt.xlabel('f')
-# plt.ylabel('rR\'C')
-# plt.plot(all_f, rRC)
+# plt.ylabel('rR\'R')
+# plt.plot(all_f, rRR)
+# # plt.subplot(122)
+# # plt.xlabel('f')
+# # plt.ylabel('rR\'C')
+# # plt.plot(all_f, rRC)
 # plt.show()
